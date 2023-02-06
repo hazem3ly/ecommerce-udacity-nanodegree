29f001e09602d5677b797221780f468891c582ad
Hazem ali <hazem.ali.goma@gmail.com>
2023-02-06 02:08:19 +0200

    create migrations

--- migrations/sqls/20230204165004-product-table-up.sql
+++
@@ -1,2 +0,0 @@
-/* Replace with your SQL commands */
-CREATE TABLE product(id SERIAL PRIMARY KEY,name VARCHAR(50)) 
---
+++ migrations/sqls/20230205233336-orders-table-up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE orders(id SERIAL PRIMARY KEY,product_id integer REFERENCES products(id), quantity integer,user_id integer REFERENCES users(id), status VARCHAR(50));
